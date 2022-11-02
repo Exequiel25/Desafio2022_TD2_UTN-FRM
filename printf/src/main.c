ff28416c43baa2567748f58f95716edbb934f2c1
Exequiel25 <exequiel.juan.magni@gmail.com>
2022-11-01 23:30:14 -0300

    Serial working fine

--- printf/src/main.c
+++ printf/src/main.c
@@ -4,11 +4,9 @@
 #include <string.h>
 
 // Device header file.
-#ifdef VVC_F1
-  #include "stm32f1xx.h"
-#elif VVC_L4
-  #include "stm32l4xx.h"
-#endif
+#include "stm32f1xx.h"
+
+#define BAUT_RATE 1200
 
 uint32_t SystemCoreClock = 0;
 extern uint32_t _sidata, _sdata, _edata, _sbss, _ebss;
@@ -26,13 +24,8 @@ __attribute__( ( naked ) ) void reset_handler( void ) {
 int _write( int handle, char* data, int size ) {
   int count = size;
   while( count-- ) {
-    #ifdef VVC_F1
-      while( !( USART2->SR & USART_SR_TXE ) ) {};
-      USART2->DR = *data++;
-    #elif VVC_L4
-      while( !( USART2->ISR & USART_ISR_TXE ) ) {};
-      USART2->TDR = *data++;
-    #endif
+    while( !( USART2->SR & USART_SR_TXE ) ) {};
+    USART2->DR = *data++;
   }
   return size;
 }
@@ -50,26 +43,14 @@ int main( void ) {
   SCB->CPACR    |=  ( 0xF << 20 );
 
   // Set the core system clock speed.
-  #ifdef VVC_F1
     // Default clock source is the 8MHz internal oscillator.
     SystemCoreClock = 8000000;
-  #elif VVC_L4
-    // Default clock source is the "multi-speed" internal oscillator.
-    // Switch to the 16MHz HSI oscillator.
-    RCC->CR |=  ( RCC_CR_HSION );
-    while ( !( RCC->CR & RCC_CR_HSIRDY ) ) {};
-    RCC->CFGR &= ~( RCC_CFGR_SW );
-    RCC->CFGR |=  ( RCC_CFGR_SW_HSI );
-    while ( ( RCC->CFGR & RCC_CFGR_SWS ) != RCC_CFGR_SWS_HSI ) {};
-    SystemCoreClock = 16000000;
-  #endif
 
   // Enable peripheral clocks and set up GPIO pins.
-  #ifdef VVC_F1
     // Enable peripheral clocks: GPIOA, USART2.
     RCC->APB1ENR  |=  ( RCC_APB1ENR_USART2EN );
     RCC->APB2ENR  |=  ( RCC_APB2ENR_IOPAEN );
-    // Configure pin A2(Tx) A3(Rx) for USART2.
+    // Configure pins A2, A3 for USART2.
     GPIOA->CRL    &= ~( GPIO_CRL_MODE2 |
                         GPIO_CRL_CNF2 |
                         GPIO_CRL_MODE3 |
@@ -78,35 +59,11 @@ int main( void ) {
                        ( 0x2 << GPIO_CRL_CNF2_Pos ) |
                        ( 0x0 << GPIO_CRL_MODE3_Pos ) |
                        ( 0x1 << GPIO_CRL_CNF3_Pos ) );
-  #elif VVC_L4
-    // Enable peripheral clocks: GPIOA, USART2.
-    RCC->APB1ENR1 |= ( RCC_APB1ENR1_USART2EN );
-    RCC->AHB2ENR  |= ( RCC_AHB2ENR_GPIOAEN );
-    // Configure pins A2, A15 for USART2 (AF7).
-    GPIOA->MODER    &= ~( ( 0x3 << ( 2 * 2 ) ) |
-                          ( 0x3 << ( 15 * 2 ) ) );
-    GPIOA->MODER    |=  ( ( 0x2 << ( 2 * 2 ) ) |
-                          ( 0x2 << ( 15 * 2 ) ) );
-    GPIOA->OTYPER   &= ~( ( 0x1 << 2 ) |
-                          ( 0x1 << 15 ) );
-    GPIOA->OSPEEDR  &= ~( ( 0x3 << ( 2 * 2 ) ) |
-                          ( 0x3 << ( 15 * 2 ) ) );
-    GPIOA->OSPEEDR  |=  ( ( 0x2 << ( 2 * 2 ) ) |
-                          ( 0x2 << ( 15 * 2 ) ) );
-    GPIOA->AFR[ 0 ] &= ~( ( 0xF << ( 2 * 4 ) ) );
-    GPIOA->AFR[ 0 ] |=  ( ( 0x7 << ( 2 * 4 ) ) );
-    GPIOA->AFR[ 1 ] &= ~( ( 0xF << ( ( 15 - 8 ) * 4 ) ) );
-    GPIOA->AFR[ 1 ] |=  ( ( 0x3 << ( ( 15 - 8 ) * 4 ) ) );
-  #endif
 
-  // Set the baud rate to 12000.
-  uint16_t uartdiv = SystemCoreClock / 12000;
-  #ifdef VVC_F1
+  // Set the baud rate to 1200.
+  uint16_t uartdiv = SystemCoreClock / BAUT_RATE;
     USART2->BRR = ( ( ( uartdiv / 16 ) << USART_BRR_DIV_Mantissa_Pos ) |
                     ( ( uartdiv % 16 ) << USART_BRR_DIV_Fraction_Pos ) );
-  #elif VVC_L4
-    USART2->BRR = uartdiv;
-  #endif
 
   // Enable the USART peripheral.
   USART2->CR1 |= ( USART_CR1_RE | USART_CR1_TE | USART_CR1_UE );
@@ -114,15 +71,8 @@ int main( void ) {
   // Main loop: wait for a new byte, then echo it back.
   char rxb = '\0';
   while ( 1 ) {
-    // Change for ADC read value
-    #ifdef VVC_F1
       while( !( USART2->SR & USART_SR_RXNE ) ) {};
       rxb = USART2->DR;
-    #elif VVC_L4
-      while( !( USART2->ISR & USART_ISR_RXNE ) ) {};
-      rxb = USART2->RDR;
-    #endif
-    // Transmit the byte of data and adds a new line.
     printf( "RX: %c\r\n", rxb );
   }
 }
